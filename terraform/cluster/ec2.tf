81fd5504572970973933f25b09415f7d3b86abba
Rophy Tsai <rophy@users.noreply.github.com>
2026-01-18 15:26:28 +0000

    fix: add ECR pull permissions and imagePullSecret for K8s
    - Add ECR pull IAM policy to EC2 role
    - Add imagePullSecrets and command override to status-server manifest

--- terraform/cluster/ec2.tf
+++ terraform/cluster/ec2.tf
@@ -127,6 +127,28 @@ resource "aws_iam_role_policy" "cloudwatch_metrics" {
   })
 }
 
+# ECR pull permission for container images
+resource "aws_iam_role_policy" "ecr_pull" {
+  name = "k3s-ecr-pull-policy"
+  role = aws_iam_role.ec2.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_instance_profile" "ec2" {
   name = "k3s-perf-test-ec2-profile"
   role = aws_iam_role.ec2.name
